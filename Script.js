3ceec6c403911eb4416405f7b73c62c7c35264cc
Adi Arwan Syah <arwansyah2787@gmail.com>
2024-10-22 13:28:08 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,28 +1,30 @@
 
+
 const generatePassword = (length, options) => {
     const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowercase = "abcdefghijklmnopqrstuvwxyz";
     const numbers = "0123456789";
     const specialChars = "!@#$%^&*()";
 
-    let charset = '';
-    if (options.includeUppercase) charset += uppercase;
-    if (options.includeLowercase) charset += lowercase;
-    if (options.includeNumbers) charset += numbers;
-    if (options.includeSpecialChars) charset += specialChars;
+    let charPool = '';
+    if (options.includeUppercase) charPool += uppercase;
+    if (options.includeLowercase) charPool += lowercase;
+    if (options.includeNumbers) charPool += numbers;
+    if (options.includeSpecialChars) charPool += specialChars;
 
-    if (charset.length === 0) {
+    if (charPool.length === 0) {
         throw new Error('At least one character type must be selected.');
     }
 
     let password = '';
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset[randomIndex];
+        const randomIndex = Math.floor(Math.random() * charPool.length);
+        password += charPool[randomIndex];
     }
 
     return password;
 };
+
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = { generatePassword };
